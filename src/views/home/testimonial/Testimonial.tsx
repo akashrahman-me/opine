2a2cf6ad91415160b11e170fc22a61f8ab907042
akashrahman-me <akashrahman.me@gmail.com>
2023-04-28 10:11:44 +0600

    wrwer

--- src/views/home/testimonial/Testimonial.tsx
+++ src/views/home/testimonial/Testimonial.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import testimonial from "./Testimonial.json";
+import testimonial from "./testimonial.json";
 
 function Testimonial() {
   return (
